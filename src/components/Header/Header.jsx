f888226f890c1b853667d1354c74fd787190d013
soykotazad <snigdho32@gmail.com>
2024-03-17 16:10:12 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+           <h1 className='text-5xl font-semibold bg-green-500'> Knowledge cafee</h1>
+        </div>
+    );
+};
+
+export default Header;
